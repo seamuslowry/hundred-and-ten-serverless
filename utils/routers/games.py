c5eeb6b85ef2ad8776aa9bde4174d20fa51414bb
Seamus Lowry <spl1owry3@gmail.com>
2026-03-03 22:46:15 -0500

    refactor: game routes order

--- utils/routers/games.py
+++ utils/routers/games.py
@@ -39,6 +39,50 @@ GameResponse = Union[StartedGame, CompletedGame]
 router = APIRouter(prefix="/games", tags=["Games"])
 
 
+@router.get("/{game_id}", response_model=GameResponse)
+def game_info(game_id: str, identity: Identity = Depends(get_identity)):
+    """Retrieve 110 game."""
+    game = GameService.get(game_id)
+
+    return serialize.game(game, identity.id)
+
+
+@router.get("/{game_id}/players", response_model=list[User])
+def game_players(game_id: str, _identity: Identity = Depends(get_identity)):
+    """Retrieve players in a 110 game."""
+    game = GameService.get(game_id)
+
+    people_ids = [p.identifier for p in game.ordered_players]
+
+    return [serialize.user(u) for u in UserService.by_identifiers(people_ids)]
+
+
+@router.post("/search", response_model=list[GameResponse])
+def search_games(body: SearchGamesRequest, identity: Identity = Depends(get_identity)):
+    """Search for games"""
+    return [
+        serialize.game(g, identity.id)
+        for g in GameService.search(
+            SearchGame(
+                name=body.searchText,
+                client=identity.id,
+                statuses=body.statuses,
+                active_player=body.activePlayer,
+                winner=body.winner,
+            ),
+            body.max,
+        )
+    ]
+
+
+@router.get("/{game_id}/suggestion", response_model=SuggestionResponse)
+def suggestion(game_id: str, identity: Identity = Depends(get_identity)):
+    """Ask for a suggestion in a 110 game"""
+    game = GameService.get(game_id)
+
+    return serialize.suggestion(game.suggestion(), identity.id)
+
+
 @router.post("/{game_id}/bid", response_model=GameResponse)
 def bid(game_id: str, body: BidRequest, identity: Identity = Depends(get_identity)):
     """Bid in a 110 game"""
@@ -46,6 +90,36 @@ def bid(game_id: str, body: BidRequest, identity: Identity = Depends(get_identit
     initial_event_knowledge = len(game.events)
 
     game.act(Bid(identity.id, BidAmount(body.amount)))
+
+    game = GameService.save(game)
+
+    return serialize.game(game, identity.id, initial_event_knowledge)
+
+
+@router.post("/{game_id}/unpass", response_model=GameResponse)
+def rescind_prepass(game_id: str, identity: Identity = Depends(get_identity)):
+    """Unpass in a 110 game"""
+    game = GameService.get(game_id)
+    initial_event_knowledge = len(game.events)
+
+    game.act(Unpass(identity.id))
+
+    game = GameService.save(game)
+
+    return serialize.game(game, identity.id, initial_event_knowledge)
+
+
+@router.post("/{game_id}/select", response_model=GameResponse)
+def select_trump(
+    game_id: str,
+    body: SelectTrumpRequest,
+    identity: Identity = Depends(get_identity),
+):
+    """Select trump in a 110 game"""
+    game = GameService.get(game_id)
+    initial_event_knowledge = len(game.events)
+
+    game.act(SelectTrump(identity.id, SelectableSuit[body.suit.value]))
 
     game = GameService.save(game)
 
@@ -72,36 +146,6 @@ def discard(
     return serialize.game(game, identity.id, initial_event_knowledge)
 
 
-@router.get("/{game_id}", response_model=GameResponse)
-def game_info(game_id: str, identity: Identity = Depends(get_identity)):
-    """Retrieve 110 game."""
-    game = GameService.get(game_id)
-
-    return serialize.game(game, identity.id)
-
-
-@router.get("/{game_id}/players", response_model=list[User])
-def game_players(game_id: str, _identity: Identity = Depends(get_identity)):
-    """Retrieve players in a 110 game."""
-    game = GameService.get(game_id)
-
-    people_ids = [p.identifier for p in game.ordered_players]
-
-    return [serialize.user(u) for u in UserService.by_identifiers(people_ids)]
-
-
-@router.post("/{game_id}/leave", response_model=GameResponse)
-def leave_game(game_id: str, identity: Identity = Depends(get_identity)):
-    """Leave a 110 game (automates the player)"""
-    game = GameService.get(game_id)
-    initial_event_knowledge = len(game.events)
-
-    game.leave(identity.id)
-    game = GameService.save(game)
-
-    return serialize.game(game, identity.id, initial_event_knowledge)
-
-
 @router.post("/{game_id}/play", response_model=GameResponse)
 def play(game_id: str, body: PlayRequest, identity: Identity = Depends(get_identity)):
     """Play a card in a 110 game"""
@@ -120,57 +164,13 @@ def play(game_id: str, body: PlayRequest, identity: Identity = Depends(get_ident
     return serialize.game(game, identity.id, initial_event_knowledge)
 
 
-@router.post("/{game_id}/unpass", response_model=GameResponse)
-def rescind_prepass(game_id: str, identity: Identity = Depends(get_identity)):
-    """Unpass in a 110 game"""
+@router.post("/{game_id}/leave", response_model=GameResponse)
+def leave_game(game_id: str, identity: Identity = Depends(get_identity)):
+    """Leave a 110 game (automates the player)"""
     game = GameService.get(game_id)
     initial_event_knowledge = len(game.events)
 
-    game.act(Unpass(identity.id))
-
+    game.leave(identity.id)
     game = GameService.save(game)
 
     return serialize.game(game, identity.id, initial_event_knowledge)
-
-
-@router.post("/search", response_model=list[GameResponse])
-def search_games(body: SearchGamesRequest, identity: Identity = Depends(get_identity)):
-    """Search for games"""
-    return [
-        serialize.game(g, identity.id)
-        for g in GameService.search(
-            SearchGame(
-                name=body.searchText,
-                client=identity.id,
-                statuses=body.statuses,
-                active_player=body.activePlayer,
-                winner=body.winner,
-            ),
-            body.max,
-        )
-    ]
-
-
-@router.post("/{game_id}/select", response_model=GameResponse)
-def select_trump(
-    game_id: str,
-    body: SelectTrumpRequest,
-    identity: Identity = Depends(get_identity),
-):
-    """Select trump in a 110 game"""
-    game = GameService.get(game_id)
-    initial_event_knowledge = len(game.events)
-
-    game.act(SelectTrump(identity.id, SelectableSuit[body.suit.value]))
-
-    game = GameService.save(game)
-
-    return serialize.game(game, identity.id, initial_event_knowledge)
-
-
-@router.get("/{game_id}/suggestion", response_model=SuggestionResponse)
-def suggestion(game_id: str, identity: Identity = Depends(get_identity)):
-    """Ask for a suggestion in a 110 game"""
-    game = GameService.get(game_id)
-
-    return serialize.suggestion(game.suggestion(), identity.id)
