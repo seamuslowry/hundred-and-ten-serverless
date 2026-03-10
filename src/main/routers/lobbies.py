433b29f6247bc4644c199907bf6d44ec0631c09b
Seamus Lowry <spl1owry3@gmail.com>
2026-03-09 23:29:04 -0400

    fix that breaks tests

--- src/main/routers/lobbies.py
+++ src/main/routers/lobbies.py
@@ -46,14 +46,7 @@ async def lobby_players(lobby_id: str):
 
     people_ids = [p.identifier for p in lobby.ordered_players]
 
-    users = await UserService.by_identifiers(people_ids)
-    by_identifier = {u.identifier: u for u in users}
-
-    return [
-        serialize.user(by_identifier[i])
-        for i in dict.fromkeys(people_ids)
-        if i in by_identifier
-    ]
+    return [serialize.user(u) for u in await UserService.by_identifiers(people_ids)]
 
 
 @router.post("/search", response_model=list[WaitingGame])
