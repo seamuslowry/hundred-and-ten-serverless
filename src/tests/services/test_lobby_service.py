7b9163648167672cecd9c87dfcb578f8e3974110
Seamus Lowry <spl1owry3@gmail.com>
2026-03-10 21:54:59 -0400

    still use a class

--- src/tests/services/test_lobby_service.py
+++ src/tests/services/test_lobby_service.py
@@ -13,63 +13,61 @@ def _make_lobby(name: str = "") -> Lobby:
     return Lobby(name=name, organizer=Human("p1"))
 
 
-async def test_save_lobby():
-    """Lobby can be saved to the DB and receives a generated id"""
-    lobby = _make_lobby()
+class TestLobbyService:
+    """Unit tests to ensure lobby service works as expected"""
 
-    assert lobby.id is None
-    lobby = await LobbyService.save(lobby)
-    assert lobby.id is not None
+    async def test_save_lobby(self):
+        """Lobby can be saved to the DB and receives a generated id"""
+        lobby = _make_lobby()
 
+        assert lobby.id is None
+        lobby = await LobbyService.save(lobby)
+        assert lobby.id is not None
 
-async def test_get_lobby():
-    """Lobby can be retrieved from the DB"""
-    original_lobby = _make_lobby()
-    original_lobby = await LobbyService.save(original_lobby)
-    assert original_lobby.id
-    lobby = await LobbyService.get(original_lobby.id)
+    async def test_get_lobby(self):
+        """Lobby can be retrieved from the DB"""
+        original_lobby = _make_lobby()
+        original_lobby = await LobbyService.save(original_lobby)
+        assert original_lobby.id
+        lobby = await LobbyService.get(original_lobby.id)
 
-    assert lobby is not None
-    assert lobby.id == original_lobby.id
+        assert lobby is not None
+        assert lobby.id == original_lobby.id
 
+    async def test_get_non_existent_lobby(self):
+        """Unknown lobby cannot be retrieved from the DB"""
+        with pytest.raises(ValueError):
+            await LobbyService.get(str(ObjectId()))
 
-async def test_get_non_existent_lobby():
-    """Unknown lobby cannot be retrieved from the DB"""
-    with pytest.raises(ValueError):
-        await LobbyService.get(str(ObjectId()))
+    async def test_search_lobby(self):
+        """Lobbies can be searched in the DB"""
+        text = f"search_test{ObjectId()}"
+        lobbies = []
+        for i in range(5):
+            lobbies.append(await LobbyService.save(_make_lobby(name=f"{text} {i}")))
 
+        found_lobbies = await LobbyService.search(
+            "p1",
+            SearchLobbiesRequest(search_text=text, limit=len(lobbies) + 1),
+        )
 
-async def test_search_lobby():
-    """Lobbies can be searched in the DB"""
-    text = f"search_test{ObjectId()}"
-    lobbies = []
-    for i in range(5):
-        lobbies.append(await LobbyService.save(_make_lobby(name=f"{text} {i}")))
+        assert len(found_lobbies) == len(lobbies)
 
-    found_lobbies = await LobbyService.search(
-        "p1",
-        SearchLobbiesRequest(search_text=text, limit=len(lobbies) + 1),
-    )
+    async def test_start_game(self):
+        """Lobby can be converted to a game"""
+        lobby = _make_lobby()
+        lobby.join(Human("p2"))
+        lobby = await LobbyService.save(lobby)
 
-    assert len(found_lobbies) == len(lobbies)
+        game = await LobbyService.start_game(lobby)
 
+        assert game is not None
+        assert game.id == lobby.id
 
-async def test_start_game():
-    """Lobby can be converted to a game"""
-    lobby = _make_lobby()
-    lobby.join(Human("p2"))
-    lobby = await LobbyService.save(lobby)
+    async def test_start_game_requires_minimum_players(self):
+        """Lobby cannot be converted to a game with fewer than 2 players"""
+        lobby = _make_lobby()
+        await LobbyService.save(lobby)
 
-    game = await LobbyService.start_game(lobby)
-
-    assert game is not None
-    assert game.id == lobby.id
-
-
-async def test_start_game_requires_minimum_players():
-    """Lobby cannot be converted to a game with fewer than 2 players"""
-    lobby = _make_lobby()
-    await LobbyService.save(lobby)
-
-    with pytest.raises(ValueError):
-        await LobbyService.start_game(lobby)
+        with pytest.raises(ValueError):
+            await LobbyService.start_game(lobby)
